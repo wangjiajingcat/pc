e004aa3686b53e9b6156f85b4a509c080b5b0505
wangjiajingcat <44891841+wangjiajingcat@users.noreply.github.com>
2018-11-24 16:32:50 +0800

    Add files via upload

---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+    export default {
+        name: "app.vue"
+    }
+</script>
+
+<style scoped>
+
+
+</style>
